2b3b2a6d193180d4c31a7ef2dd9263c8439da25a
Clarence <clarencedo@126.com>
2024-09-02 22:30:38 +0800

    update 0034

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # leetcode-golang
-leetcode-implementation
+## [感谢halfrost的leetcode开源项目](https://github.com/halfrost/LeetCode-Go)
+
+本仓库只是记录我在学习算法中的一些笔记和代码
---
+++ question/leetcode-0034/leetcode-0034.go
@@ -0,0 +1,45 @@
+package question
+
+func searchRange(nums []int, target int) []int {
+	leftIndex := leftBound(nums, target)
+	rightIndex := rightBound(nums, target)
+	return []int{leftIndex, rightIndex}
+}
+
+func leftBound(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else if nums[mid] == target {
+			// 别返回，继续往左边找，锁定左侧边界
+			right = mid - 1
+		}
+	}
+	if left < len(nums) && nums[left] == target {
+		return left
+	}
+	return -1
+}
+
+func rightBound(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else if nums[mid] == target {
+			// 别返回，继续往右边找，锁定右侧边界
+			left = mid + 1
+		}
+	}
+	if right >= 0 && nums[right] == target {
+		return right
+	}
+	return -1
+}
