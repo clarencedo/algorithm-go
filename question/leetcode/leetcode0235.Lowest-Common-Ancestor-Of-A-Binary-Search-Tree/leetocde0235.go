28181d3f4b381d74a4313198d1178464b76a1e3b
clarence <clarencedo@126.com>
2024-09-13 13:45:54 +0800

    feat: lowest common ancestor of a binary tree

---
+++ question/leetcode/leetcode0235.Lowest-Common-Ancestor-Of-A-Binary-Search-Tree/leetocde0235.go
@@ -0,0 +1,17 @@
+package leetcode
+
+
+type TreeNode  = structure.TreeNode
+
+func lowestCommonAncestor235(root, p, q *TreeNode) *TreeNode {
+	if root == nil || root == p || root == q{
+		return root
+	}
+	if p.Val < root.Val && q.Val < root.Val{
+		return lowestCommonAncestor235(root.Left, p, q)
+	}
+	if p.Val > root.Val && q.Val > root.Val{
+		return lowestCommonAncestor235(root.Right, p, q)
+	}
+	return root
+}
---
+++ question/leetcode/leetcode0236.Lowest-Common-Ancestor-Of-A-Binary-Tree/leetcode0236.go
@@ -0,0 +1,20 @@
+package leetcode
+
+
+func lowestCommonAncestor236(root, p, q *TreeNode) *TreeNode {
+     if root == nil || root == p || root == q{
+         return root
+     }
+     left := lowestCommonAncestor(root.Left, p ,q)
+     right := lowestCommonAncestor(root.Right, p ,q)
+     if left == nil && right == nil {
+         return nil
+     }
+     if left == nil {
+         return right
+     }
+     if right == nil {
+         return left
+     }
+     return root
+}
