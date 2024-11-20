7a607d5733c401b5a00773ac69091059cf780d46
Clarence <clarencedo@126.com>
2024-11-21 00:45:42 +0800

    update sort template

---
+++ template/quicksort.go
@@ -0,0 +1,22 @@
+package template
+
+func QuickSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	pivot := arr[len(arr)-1]
+	var left, right []int
+
+	for i := 0; i < len(arr)-1; i++ {
+		if arr[i] < pivot {
+			left = append(left, arr[i])
+		} else {
+			right = append(right, arr[i])
+		}
+	}
+	left = QuickSort(left)
+	right = QuickSort(right)
+
+	return append(append(left, pivot), right...)
+}
